16ed769d9ca9345cdaed660e2cc61b6ff9db2baf
Elvan H <elvanhd@gmail.com>
2021-04-08 00:32:03 +0700

    template inheritance and layouts

--- laravel8/routes/web.php
+++ laravel8/routes/web.php
@@ -11,14 +11,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('home.index');
 })->name('home.index');
 
 Route::get('/contact', function () {
-    return 'Contact';
+    return view('home.contact');
 })->name('home.contact');
 
 Route::get('/posts/{id}', function ($id) {
