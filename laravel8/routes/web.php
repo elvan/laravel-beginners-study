b8873f8389ee20593e5d5b959c0884d1d95c5bce
Elvan H <elvanhd@gmail.com>
2021-04-08 09:56:35 +0700

    passing and rendering data in templates

--- laravel8/routes/web.php
+++ laravel8/routes/web.php
@@ -22,7 +22,20 @@ Route::get('/contact', function () {
 })->name('home.contact');
 
 Route::get('/posts/{id}', function ($id) {
-    return 'Blog ' . $id;
+    $posts = [
+        1 => [
+            'title' => 'Intro to Laravel',
+            'content' => 'This is a short intro to Laravel'
+        ],
+        2 => [
+            'title' => 'Intro to PHP',
+            'content' => 'This is a short intro to PHP'
+        ]
+    ];
+
+    abort_if(!isset($posts[$id]), 404);
+
+    return view('posts.show', ['post' => $posts[$id]]);
 })->name('posts.show');
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
