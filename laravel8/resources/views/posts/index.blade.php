778b76dca66103810d8f285c3f3ce8ccf132bf0a
Elvan H <elvanhd@gmail.com>
2021-04-08 10:45:04 +0700

    partial templates

--- laravel8/resources/views/posts/index.blade.php
+++ laravel8/resources/views/posts/index.blade.php
@@ -12,14 +12,7 @@
     @endif --}}
 
     @forelse ($posts as $key => $post)
-        {{-- @break($key == 2) --}}
-        {{-- @continue($key == 1) --}}
-        @if ($loop->even)
-            <div>{{ $key }}. {{ $post['title'] }}</div>
-        @else
-            <div style="background-color: silver">{{ $key }}. {{ $post['title'] }}</div>
-        @endif
-
+        @include('posts.partials.post')
     @empty
         No posts found!
     @endforelse
