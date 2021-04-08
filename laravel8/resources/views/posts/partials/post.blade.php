0061889c130d21aa9518647a6e5e4e73a785005c
Elvan H <elvanhd@gmail.com>
2021-04-08 10:48:30 +0700

    partial templates in loops

--- laravel8/resources/views/posts/partials/post.blade.php
+++ laravel8/resources/views/posts/partials/post.blade.php
@@ -1,5 +1,8 @@
 {{-- @break($key == 2) --}}
 {{-- @continue($key == 1) --}}
+
+{{-- <div>{{ $key }}. {{ $post['title'] }}</div> --}}
+
 @if ($loop->even)
     <div>{{ $key }}. {{ $post['title'] }}</div>
 @else
