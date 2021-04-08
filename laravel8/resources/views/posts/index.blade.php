e21c559300993e7335bf705c96a8d73d28a9e7cd
Elvan H <elvanhd@gmail.com>
2021-04-08 10:39:43 +0700

    more control inside loops

--- laravel8/resources/views/posts/index.blade.php
+++ laravel8/resources/views/posts/index.blade.php
@@ -3,7 +3,24 @@
 @section('title', 'Blog Posts')
 
 @section('content')
-    @foreach ($posts as $key => $post)
-        <div>{{ $key }}. {{ $post['title'] }}</div>
-    @endforeach
+    {{-- @if (count($posts))
+        @foreach ($posts as $key => $post)
+            <div>{{ $key }}. {{ $post['title'] }}</div>
+        @endforeach
+    @else
+        No posts found!
+    @endif --}}
+
+    @forelse ($posts as $key => $post)
+        {{-- @break($key == 2) --}}
+        {{-- @continue($key == 1) --}}
+        @if ($loop->even)
+            <div>{{ $key }}. {{ $post['title'] }}</div>
+        @else
+            <div style="background-color: silver">{{ $key }}. {{ $post['title'] }}</div>
+        @endif
+
+    @empty
+        No posts found!
+    @endforelse
 @endsection
