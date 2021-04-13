3be3815eb628f2f3fdd645da087dd021de4e781c
Elvan H <elvanhd@gmail.com>
2021-04-13 23:54:48 +0700

    returning file downloads

--- laravel8/routes/web.php
+++ laravel8/routes/web.php
@@ -88,3 +88,7 @@ Route::get('/fun/away', function () {
 Route::get('/fun/json', function () use ($posts) {
   return response()->json($posts);
 });
+
+Route::get('/fun/download', function () {
+  return response()->download(public_path('/favicon.ico'));
+});
