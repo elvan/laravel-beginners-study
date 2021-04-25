ff3184905fde3004d78b6d5c30412a0a43f81c0d
Elvan H <elvanhd@gmail.com>
2021-04-25 14:51:41 +0700

    including assets in views

--- laravel8/resources/views/layouts/app.blade.php
+++ laravel8/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel App @yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
@@ -17,6 +18,8 @@
         @endif
         @yield('content')
     </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
