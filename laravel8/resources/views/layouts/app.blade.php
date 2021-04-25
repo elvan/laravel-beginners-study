7d806d003b0ae39417173d686c5faebed5c3a766
Elvan H <elvanhd@gmail.com>
2021-04-25 16:59:50 +0700

    styling flash messages and error messages

--- laravel8/resources/views/layouts/app.blade.php
+++ laravel8/resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     </div>
     <div class="container">
         @if (session('status'))
-            <div style="background: red; color: white">
+            <div class="alert alert-success">
                 {{ session('status') }}
             </div>
         @endif
--- laravel8/resources/views/posts/partials/form.blade.php
+++ laravel8/resources/views/posts/partials/form.blade.php
@@ -3,18 +3,18 @@
     <input class="form-control" name="title" type="text" value="{{ old('title', optional($post ?? null)->title) }}">
 </div>
 @error('title')
-    <div>{{ $message }}</div>
+    <div class="alert alert-danger">{{ $message }}</div>
 @enderror
 <div class="form-group">
     <label for="content">Content</label>
     <textarea class="form-control" name="content" id="content"
-        rows="10">{{ old('content', optional($post ?? null)->content) }}</textarea>
+        rows="15">{{ old('content', optional($post ?? null)->content) }}</textarea>
 </div>
 @if ($errors->any())
-    <div>
-        <ul>
+    <div class="mb-3">
+        <ul class="list-group">
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li class="list-group-item list-group-item-danger">{{ $error }}</li>
             @endforeach
         </ul>
     </div>
