34ac64b8ca7307155104a559dd77b9368ad86dbd
Elvan H <elvanhd@gmail.com>
2021-04-25 18:44:27 +0700

    testing database interactions

--- laravel8/resources/views/posts/index.blade.php
+++ laravel8/resources/views/posts/index.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Blog Posts')
 
 @section('content')
-  @forelse ($posts as $key => $post)
-    @include('posts.partials.post')
-  @empty
-    No posts found!
-  @endforelse
+    @forelse ($posts as $key => $post)
+        @include('posts.partials.post')
+    @empty
+        No blog posts yet!
+    @endforelse
 @endsection
