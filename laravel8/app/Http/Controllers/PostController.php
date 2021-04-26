bde00a46927e33f140c5333fc7034e87e14803a9
Elvan H <elvanhd@gmail.com>
2021-04-26 14:48:48 +0700

    lazy loading example

--- laravel8/app/Http/Controllers/PostController.php
+++ laravel8/app/Http/Controllers/PostController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePost;
 use App\Models\BlogPost;
-use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\DB;
 
-class PostsController extends Controller
+class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,6 +16,18 @@ class PostsController extends Controller
      */
     public function index()
     {
+        // DB::connection()->enableQueryLog();
+
+        // $posts = BlogPost::with('comments')->get();
+
+        // foreach ($posts as $post) {
+        //     foreach ($post->comments as $comment) {
+        //         echo $comment->content;
+        //     }
+        // }
+
+        // dd(DB::getQueryLog());
+
         return view('posts.index', ['posts' => BlogPost::all()]);
     }
 
