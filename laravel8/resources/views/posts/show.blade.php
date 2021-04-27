e0bc975d053fc2f0d8176998dc2090c05bbce3c2
Elvan H <elvanhd@gmail.com>
2021-04-27 23:35:55 +0700

    implementing comment list

--- laravel8/resources/views/posts/show.blade.php
+++ laravel8/resources/views/posts/show.blade.php
@@ -10,4 +10,16 @@
     @if (now()->diffInMinutes($post->created_at) < 5)
         <div class="alert alert-info">New</div>
     @endif
+
+    <h4>Comments</h4>
+    <div>
+        @forelse ($post->comments as $comment)
+            <p>{{ $comment->content }}</p>
+            <p class="muted"> added {{ $comment->created_at->diffForHumans() }}</p>
+            <hr>
+        @empty
+            <p>No comments yet!</p>
+        @endforelse
+    </div>
+
 @endsection
