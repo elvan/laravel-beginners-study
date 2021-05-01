8b6f3a8b77d57774c1c6ba4ae4284b18e52c7043
Elvan H <elvanhd@gmail.com>
2021-05-01 23:52:46 +0700

    individual seeder classes

--- laravel8/database/seeders/DatabaseSeeder.php
+++ laravel8/database/seeders/DatabaseSeeder.php
@@ -16,19 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $example = User::factory()->exampleUser()->create();
-        $other = User::factory()->count(20)->create();
-
-        $users = $other->concat([$example]);
-
-        $posts = BlogPost::factory()->count(50)->make()->each(function ($post) use ($users) {
-            $post->user_id = $users->random()->id;
-            $post->save();
-        });
-
-        Comment::factory()->count(150)->make()->each(function ($comment) use ($posts) {
-            $comment->blog_post_id = $posts->random()->id;
-            $comment->save();
-        });
+        $this->call([
+            UserSeeder::class,
+            BlogPostSeeder::class,
+            CommentSeeder::class,
+        ]);
     }
 }
