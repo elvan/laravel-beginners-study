9461a71f56a09975efd24827cb3d40543aee203d
Elvan H <elvanhd@gmail.com>
2021-05-01 23:00:42 +0700

    database seeding

--- laravel8/database/seeders/DatabaseSeeder.php
+++ laravel8/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Example User',
+            'email' => 'user@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('pswd1234'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
