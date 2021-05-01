0e5a037c256982c10f0144212a5ce4afbb825b33
Elvan H <elvanhd@gmail.com>
2021-05-01 08:30:44 +0700

    formatting validation errors

--- laravel8/resources/views/auth/register.blade.php
+++ laravel8/resources/views/auth/register.blade.php
@@ -6,17 +6,38 @@
 
         <div class="form-group">
             <label for="name">Name</label>
-            <input class="form-control" name="name" required type="text" value="">
+            <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="name" required type="text"
+                value="">
+
+            @if ($errors->has('name'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+            @endif
         </div>
 
         <div class="form-group">
             <label for="email">Email</label>
-            <input class="form-control" name="email" required type="email" value="">
+            <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="email" required type="email"
+                value="">
+
+            @if ($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
         </div>
 
         <div class="form-group">
             <label for="password">Password</label>
-            <input class="form-control" name="password" required type="password" value="">
+            <input class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" required
+                type="password" value="">
+
+            @if ($errors->has('password'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
         </div>
 
         <div class="form-group">
