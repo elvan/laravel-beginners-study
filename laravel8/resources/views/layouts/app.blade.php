fd0f9f976ded82ccb6eccb4275fac6c5efe02ffd
Elvan H <elvanhd@gmail.com>
2021-05-01 09:08:10 +0700

    retrieving the currently authenticated user

--- laravel8/resources/views/layouts/app.blade.php
+++ laravel8/resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
                 <a class="p-2 text-dark" href="{{ route('login') }}">Login</a>
             @else
                 <a class="p-2 text-dark" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
+                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                    Logout ({{ Auth::user()->name }})</a>
                 <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none;">
                     @csrf
                 </form>
