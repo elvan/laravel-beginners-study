69646545fd8c6267406b3c1710b2f21b63051afc
Elvan H <elvanhd@gmail.com>
2021-05-03 06:32:50 +0700

    updating tests

--- laravel8/tests/Feature/ExampleFeatureTest.php
+++ laravel8/tests/Feature/ExampleFeatureTest.php
@@ -2,10 +2,9 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ExampleFeatureTest extends TestCase
 {
     /**
      * A basic test example.
