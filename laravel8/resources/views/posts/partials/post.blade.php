39eb241ebd750939b77ff999fa2fc9da03f64856
Elvan H <elvanhd@gmail.com>
2021-05-03 17:10:15 +0700

    blade components

--- laravel8/resources/views/posts/partials/post.blade.php
+++ laravel8/resources/views/posts/partials/post.blade.php
@@ -9,17 +9,25 @@
         </del>
     @endif
 </h3>
-<p class="text-muted">Added {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}</p>
+<p class="text-muted">
+    Added {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}
+</p>
 
 @if ($post->comments_count)
-    <p>{{ $post->comments_count }} comments</p>
+    <p>
+        {{ $post->comments_count }} comments
+    </p>
 @else
-    <p>No comments yet</p>
+    <p>
+        No comments yet
+    </p>
 @endif
 
 <div class="mb-3">
     @can('update', $post)
-        <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
+        <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post->id]) }}">
+            Edit
+        </a>
     @endcan
     @can('delete', $post)
         @unless($post->trashed())
