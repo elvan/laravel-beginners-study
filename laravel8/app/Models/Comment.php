b86bc58d9743e4062e2a3d48434ba702dd49730e
Elvan H <elvanhd@gmail.com>
2021-05-03 08:21:35 +0700

    adding global latest scope to the comment model

--- laravel8/app/Models/Comment.php
+++ laravel8/app/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\BlogPost;
+use App\Scopes\LatestScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,5 +16,12 @@ class Comment extends Model
     public function blogPost()
     {
         return $this->belongsTo(BlogPost::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new LatestScope());
     }
 }
