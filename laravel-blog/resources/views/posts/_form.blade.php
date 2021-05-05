66f136e16942623afe81e0f7b7cba07e3bec61c0
Elvan H <elvanhd@gmail.com>
2021-05-05 22:45:42 +0700

    comments form and reusable errors component

--- laravel-blog/resources/views/posts/_form.blade.php
+++ laravel-blog/resources/views/posts/_form.blade.php
@@ -1,25 +1,12 @@
 <div class="form-group">
     <label for="title">Title</label>
-    <input class="form-control" name="title" type="text" value="{{ old('title', optional($post ?? null)->title) }}">
+    <input class="form-control @error('title') is-invalid @enderror" name="title" type="text"
+        value="{{ old('title', optional($post ?? null)->title) }}">
 </div>
-@error('title')
-    <div class="alert alert-danger">{{ $message }}</div>
-@enderror
 <div class="form-group">
     <label for="content">Content</label>
-    <textarea class="form-control" name="content" id="content"
+    <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content"
         rows="15">{{ old('content', optional($post ?? null)->content) }}</textarea>
 </div>
-@error('content')
-    <div class="alert alert-danger">{{ $message }}</div>
-@enderror
 
-{{-- @if ($errors->any())
-    <div class="mb-3">
-        <ul class="list-group">
-            @foreach ($errors->all() as $error)
-                <li class="list-group-item list-group-item-danger">{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif --}}
+<x-errors></x-errors>
