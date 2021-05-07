8e8a7cd1eafcb4424c461089fb8139738bd19b19
Elvan H <elvanhd@gmail.com>
2021-05-08 06:16:59 +0700

    attaching files and data to emails

--- laravel-blog/app/Mail/CommentPosted.php
+++ laravel-blog/app/Mail/CommentPosted.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 
 class CommentPosted extends Mailable
 {
@@ -35,6 +36,24 @@ class CommentPosted extends Mailable
 
         return $this->from('laravel-blog@example.com', 'Blog Admin')
             ->subject($subject)
+            // ->attach(
+            //     storage_path('app/public') . '/' . $this->comment->user->image->path,
+            //     [
+            //         'as' => 'profile_picture.jpg',
+            //         'mime' => 'image/jpg'
+            //     ]
+            // )
+            // ->attachFromStorage(
+            //     $this->comment->user->image->path,
+            //     'profile_picture.jpg'
+            // )
+            // ->attachData(
+            //     Storage::get($this->comment->user->image->path),
+            //     'profile_picture_from_data.jpg',
+            //     [
+            //         'mime' => 'image/jpeg'
+            //     ]
+            // )
             ->view('emails.posts.commented');
     }
 }
