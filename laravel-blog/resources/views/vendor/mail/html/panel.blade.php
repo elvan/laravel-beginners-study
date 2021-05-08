ccf947c01cefec5241939927bd45b77b9eadd1cc
Elvan H <elvanhd@gmail.com>
2021-05-08 07:33:39 +0700

    custom markdown email component and styling

--- laravel-blog/resources/views/vendor/mail/html/panel.blade.php
+++ laravel-blog/resources/views/vendor/mail/html/panel.blade.php
@@ -3,9 +3,7 @@
         <td class="panel-content">
             <table width="100%" cellpadding="0" cellspacing="0" role="presentation">
                 <tr>
-                    <td class="panel-item">
-                        {{ Illuminate\Mail\Markdown::parse($slot) }}
-                    </td>
+                    <td class="panel-item">{{ Illuminate\Mail\Markdown::parse($slot) }}</td>
                 </tr>
             </table>
         </td>
