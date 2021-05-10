ccda6726b510ad022df39fe9134f976d64b6515b
Elvan H <elvanhd@gmail.com>
2021-05-10 10:21:37 +0700

    translating plural forms, passing data

---
+++ laravel-blog/resources/lang/id/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'welcome' => 'Selamat datang ke Laravel Blog!',
+    'example_with_value' => 'Halo, :name',
+    'plural' => '{0} Tidak ada komentar|[1,2] :count komentar',
+];
--- laravel-blog/resources/views/home/index.blade.php
+++ laravel-blog/resources/views/home/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home Page</h1>
+    <h1>{{ __('messages.welcome') }}</h1>
     <p>This is the content of the Home Page</p>
+
+    <p>{{ __('messages.example_with_value', ['name' => 'Someuser']) }}</p>
+
+    <p>{{ trans_choice('messages.plural', 0) }}</p>
+    <p>{{ trans_choice('messages.plural', 1) }}</p>
+    <p>{{ trans_choice('messages.plural', 2) }}</p>
 @endsection
