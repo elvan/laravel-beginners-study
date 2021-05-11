40f507bdf7435f77e550d5b81cc26bcc9ccc7b8d
Elvan H <elvanhd@gmail.com>
2021-05-11 10:32:20 +0700

    storing user preffered language in database

--- laravel-blog/resources/views/home/index.blade.php
+++ laravel-blog/resources/views/home/index.blade.php
@@ -3,10 +3,4 @@
 @section('content')
     <h1>{{ __('Welcome to Laravel Blog') }}</h1>
     <p>This is the content of the Home Page</p>
-
-    <p>{{ __('Hello, :name', ['name' => 'Someuser']) }}</p>
-
-    <p>{{ trans_choice('messages.plural', 0) }}</p>
-    <p>{{ trans_choice('messages.plural', 1) }}</p>
-    <p>{{ trans_choice('messages.plural', 2) }}</p>
 @endsection
