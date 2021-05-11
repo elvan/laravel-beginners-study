61c311bcaba3e47fb27167629d5edfd3ad3d2ab0
Elvan H <elvanhd@gmail.com>
2021-05-11 10:14:37 +0700

    translating the application

--- laravel-blog/resources/lang/id/messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'welcome' => 'Selamat datang ke Laravel Blog!',
-    'example_with_value' => 'Halo, :name',
-    'plural' => '{0} Tidak ada komentar|[1,2] :count komentar',
-];
