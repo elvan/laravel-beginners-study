4c96f36744dfeb81cd3437a08fa712787231fe1a
Elvan H <elvanhd@gmail.com>
2021-05-12 19:04:09 +0700

    testing api get methods

---
+++ laravel-blog/tests/Feature/ApiPostCommentsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\BlogPost;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiPostCommentsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_new_blog_post_does_not_have_comments()
+    {
+        BlogPost::factory()->create([
+            'user_id' => $this->user()->id,
+        ]);
+
+        $response = $this->json('GET', '/api/v1/posts/1/comments');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data', 'links', 'meta'])
+            ->assertJsonCount(0, 'data');
+    }
+}
