e3c91af18ead6a975d951730f5e6c40155c32cf6
Elvan H <elvanhd@gmail.com>
2021-05-12 19:27:31 +0700

    verifying json structure

--- laravel-blog/tests/Feature/ApiPostCommentsTest.php
+++ laravel-blog/tests/Feature/ApiPostCommentsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\BlogPost;
+use App\Models\Comment;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,11 +12,6 @@ class ApiPostCommentsTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     public function test_new_blog_post_does_not_have_comments()
     {
         BlogPost::factory()->create([
@@ -27,5 +23,41 @@ class ApiPostCommentsTest extends TestCase
         $response->assertStatus(200)
             ->assertJsonStructure(['data', 'links', 'meta'])
             ->assertJsonCount(0, 'data');
+    }
+
+    public function test_blog_post_has_ten_comments()
+    {
+        $blogPost = BlogPost::factory()->create([
+            'user_id' => $this->user()->id,
+        ]);
+
+        $blogPost->each(function ($post) {
+            $post->comments()->saveMany(
+                Comment::factory()->count(10)->make([
+                    'user_id' => $this->user()->id,
+                ])
+            );
+        });
+
+        $response = $this->json('GET', '/api/v1/posts/2/comments');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'content',
+                        'created_at',
+                        'updated_at',
+                        'user' => [
+                            'id',
+                            'name'
+                        ]
+                    ]
+                ],
+                'links',
+                'meta'
+            ])
+            ->assertJsonCount(10, 'data');
     }
 }
